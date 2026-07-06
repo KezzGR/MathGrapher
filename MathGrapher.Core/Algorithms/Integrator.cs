7b4a2b35c50df98704b27ac6b7253814229e411a
Nikita Krasnyh <nik2005kras@gmail.com>
2026-07-06 11:16:34 +0300

    Add Trapezoidal Method

---
+++ MathGrapher.Core/Algorithms/Integrator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathGrapher.Core.Algorithms
+{
+    public static class Integrator
+    {
+        public static double Trapezoidal(Func<double, double> f, double a, double b, int n)
+        {
+            double h = (b - a) / n;
+            double sum = 0.5 * (f(a) + f(b));
+            for (int i = 1; i < n; i++)
+                sum += f(a + i * h);
+
+            return sum * h;
+        }
+    }
+}
